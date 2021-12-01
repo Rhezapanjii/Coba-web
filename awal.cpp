d7f4649e4c808c721b0fbb340986bb178f6935eb
Rheza Panji <rheza.panji.tif21@polban.ac.id>
2021-12-01 22:02:09 +0700

    tambah code

---
+++ awal.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+int main (){
+	int nilai[6]={3,2,9,4,8,7};
+	int sesuatu[6]={1,6,9,5,3,4};
+    int sempurna[12];
+	
+    for(int i=0;i<12;i++){
+        if (i>5){
+            sempurna[i]=sesuatu[i-6];
+        }
+        else{
+            sempurna[i]=nilai[i];
+        }
+    }
+    
+    for (int i=0;i<12;i++){
+    	for (int j= i+1;j<12;j++){
+        	int a=0;
+        	if(sempurna[i]>sempurna[j]){
+                 a=sempurna[i];
+            	 sempurna[i]=sempurna [j];
+            	 sempurna[j]=a;
+        	 }
+	    }
+	}
+	for(int i=0;i<12;i++){
+	    cout<<sempurna[i];
+	}
+	
+	
+	
+}
+
